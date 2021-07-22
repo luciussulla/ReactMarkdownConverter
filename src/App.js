50b8f34a61e4a8048448947718808ded0031b10a
luciussulla <maciek.kowalczyk@yandex.com>
2021-07-22 16:07:30 +0200

    Finish functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
+import React, {useState} from 'react'
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState("## Markdown preview")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text-area">
+        <form>
+          <textarea className="text-area__area"
+            onChange={(e)=>setMarkdown(e.target.value)}
+            value={markdown}
+          ></textarea>
+        </form>
+      </div>
+      <div className="markup-area">
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
     </div>
   );
 }
